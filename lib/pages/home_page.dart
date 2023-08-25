8b8304f5aa0193db0561fffa08d7b28f9427c3d8
Eder Soares Sena <eder.sena@live.com>
2023-08-25 15:08:17 -0300

    chore: Drawer e Dialog

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,9 +10,116 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Trilha Flutter"),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Trilha Flutter"),
+        ),
+        drawer: Drawer(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const UserAccountsDrawerHeader(
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      "https://avatars.githubusercontent.com/u/26800480?v=4"),
+                ),
+                accountName: Text("Eder Soares Sena"),
+                accountEmail: Text("eder.sena@live.com"),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    InkWell(
+                      child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 5),
+                          width: double.infinity,
+                          child: const Text("Dados cadastrais")),
+                      onTap: () => {
+                        showDialog<String>(
+                          context: context,
+                          builder: (BuildContext context) => AlertDialog(
+                            title: const Text('AlertDialog Title'),
+                            content: const Text('AlertDialog description'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.pop(context, 'Cancel'),
+                                child: const Text('Cancel'),
+                              ),
+                              TextButton(
+                                onPressed: () => Navigator.pop(context, 'OK'),
+                                child: const Text('OK'),
+                              ),
+                            ],
+                          ),
+                        )
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    InkWell(
+                      child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 5),
+                          width: double.infinity,
+                          child: const Text("Termos de uso e privacidade")),
+                      onTap: () => {
+                        showDialog<String>(
+                          context: context,
+                          builder: (BuildContext context) => AlertDialog(
+                            title: const Text('AlertDialog Title'),
+                            content: const Text('AlertDialog description'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.pop(context, 'Cancel'),
+                                child: const Text('Cancel'),
+                              ),
+                              TextButton(
+                                onPressed: () => Navigator.pop(context, 'OK'),
+                                child: const Text('OK'),
+                              ),
+                            ],
+                          ),
+                        )
+                      },
+                    ),
+                    const SizedBox(height: 10),
+                    const Divider(),
+                    InkWell(
+                      child: Container(
+                          padding: const EdgeInsets.symmetric(vertical: 5),
+                          width: double.infinity,
+                          child: const Text("Configurações")),
+                      onTap: () => {
+                        showDialog<String>(
+                          context: context,
+                          builder: (BuildContext context) => AlertDialog(
+                            title: const Text('AlertDialog Title'),
+                            content: const Text('AlertDialog description'),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () =>
+                                    Navigator.pop(context, 'Cancel'),
+                                child: const Text('Cancel'),
+                              ),
+                              TextButton(
+                                onPressed: () => Navigator.pop(context, 'OK'),
+                                child: const Text('OK'),
+                              ),
+                            ],
+                          ),
+                        )
+                      },
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }

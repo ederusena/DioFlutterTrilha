e03c4697f87895857c245297ae43120cd6c24b05
Eder Soares Sena <eder.sena@live.com>
2023-08-31 16:44:43 -0300

    chore: UserAccountsDrawerHeader

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:trilhapp/pages/dados_cadastrais.dart';
 import 'package:trilhapp/pages/pagina1.dart';
 import 'package:trilhapp/pages/pagina2.dart';
 import 'package:trilhapp/pages/pagina3.dart';
+import 'package:trilhapp/shared/widget/custom_drawer.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -21,98 +22,7 @@ class _MainPageState extends State<MainPage> {
         appBar: AppBar(
           title: const Text("Trilha Flutter"),
         ),
-        drawer: Drawer(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const UserAccountsDrawerHeader(
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: NetworkImage(
-                      "https://avatars.githubusercontent.com/u/26800480?v=4"),
-                ),
-                accountName: Text("Eder Soares Sena"),
-                accountEmail: Text("eder.sena@live.com"),
-              ),
-              Padding(
-                padding:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    InkWell(
-                      child: Container(
-                          padding: const EdgeInsets.symmetric(vertical: 5),
-                          width: double.infinity,
-                          child: const Text("Dados cadastrais")),
-                      onTap: () {
-                        Navigator.pop(context, 'OK');
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const DadosCadastrais()));
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    InkWell(
-                      child: Container(
-                          padding: const EdgeInsets.symmetric(vertical: 5),
-                          width: double.infinity,
-                          child: const Text("Termos de uso e privacidade")),
-                      onTap: () => {
-                        showDialog<String>(
-                          context: context,
-                          builder: (BuildContext context) => AlertDialog(
-                            title: const Text('AlertDialog Title'),
-                            content: const Text('AlertDialog description'),
-                            actions: <Widget>[
-                              TextButton(
-                                onPressed: () =>
-                                    Navigator.pop(context, 'Cancel'),
-                                child: const Text('Cancel'),
-                              ),
-                              TextButton(
-                                onPressed: () => Navigator.pop(context, 'OK'),
-                                child: const Text('OK'),
-                              ),
-                            ],
-                          ),
-                        )
-                      },
-                    ),
-                    const SizedBox(height: 10),
-                    const Divider(),
-                    InkWell(
-                      child: Container(
-                          padding: const EdgeInsets.symmetric(vertical: 5),
-                          width: double.infinity,
-                          child: const Text("Configurações")),
-                      onTap: () => {
-                        showDialog<String>(
-                          context: context,
-                          builder: (BuildContext context) => AlertDialog(
-                            title: const Text('AlertDialog Title'),
-                            content: const Text('AlertDialog description'),
-                            actions: <Widget>[
-                              TextButton(
-                                onPressed: () =>
-                                    Navigator.pop(context, 'Cancel'),
-                                child: const Text('Cancel'),
-                              ),
-                              TextButton(
-                                onPressed: () => Navigator.pop(context, 'OK'),
-                                child: const Text('OK'),
-                              ),
-                            ],
-                          ),
-                        )
-                      },
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
+        drawer: const CustomDrawer(),
         body: Column(
           children: [
             Expanded(
---
+++ lib/shared/widget/custom_drawer.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+import 'package:trilhapp/pages/dados_cadastrais.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const UserAccountsDrawerHeader(
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://avatars.githubusercontent.com/u/26800480?v=4"),
+            ),
+            accountName: Text("Eder Soares Sena"),
+            accountEmail: Text("eder.sena@live.com"),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      width: double.infinity,
+                      child: const Text("Dados cadastrais")),
+                  onTap: () {
+                    Navigator.pop(context, 'OK');
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const DadosCadastrais()));
+                  },
+                ),
+                const SizedBox(height: 10),
+                InkWell(
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      width: double.infinity,
+                      child: const Text("Termos de uso e privacidade")),
+                  onTap: () => {
+                    showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('AlertDialog Title'),
+                        content: const Text('AlertDialog description'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'Cancel'),
+                            child: const Text('Cancel'),
+                          ),
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'OK'),
+                            child: const Text('OK'),
+                          ),
+                        ],
+                      ),
+                    )
+                  },
+                ),
+                const SizedBox(height: 10),
+                const Divider(),
+                InkWell(
+                  child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 5),
+                      width: double.infinity,
+                      child: const Text("Configurações")),
+                  onTap: () => {
+                    showDialog<String>(
+                      context: context,
+                      builder: (BuildContext context) => AlertDialog(
+                        title: const Text('AlertDialog Title'),
+                        content: const Text('AlertDialog description'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'Cancel'),
+                            child: const Text('Cancel'),
+                          ),
+                          TextButton(
+                            onPressed: () => Navigator.pop(context, 'OK'),
+                            child: const Text('OK'),
+                          ),
+                        ],
+                      ),
+                    )
+                  },
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
